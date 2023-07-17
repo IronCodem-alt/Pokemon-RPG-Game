fa95014876d1f757a90db098e019df08a0fc3ba5
Yashas S <110331815+IronCodem-alt@users.noreply.github.com>
2023-07-17 20:02:59 +0530

    Create poke_files.py

---
+++ poke_files.py
@@ -0,0 +1,25 @@
+strong = '\033[1m'
+normal = '\033[0m'
+
+yellow = '\033[38;2;255;225;0m'
+blue = '\033[38;2;0;30;255m'
+
+title = f'''
+					                                {strong}.::.
+					                             .;:**'
+					                              `{normal}
+					  {yellow}.:XHHHHk.              db.   .;;.     dH  MX 
+					oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN
+					QMMMMMb  "MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM
+					  `MMMM.  )M> :X!Hk. MMMM   XMM.o"  .  MMMMMMM X?XMMM MMM>!MMP
+					   'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `" MX MMXXMM
+					    ~MMMMM~ XMM. .XM XM`"MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP
+					     ?MMM>  YMMMMMM! MM   `?MMRb.    `"""   !L"MMMMM XM IMMM
+    					  MMMX   "MMMM"  MM       ~%:           !Mh.""" dMI IMMP
+   					  	  'MMM.                                             IMX
+    					   ~M!M                                             IMP
+
+					 		 {blue}__                                        
+	  	  					/ _  _ |_|_ _    _ _ |_ _|_   " _ _    _ || |
+		  					\__)(_)|_|_(_|  (_(_||_(_| )   (-|||  (_||| .
+'''
